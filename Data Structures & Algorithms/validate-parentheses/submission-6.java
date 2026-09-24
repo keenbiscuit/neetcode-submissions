8b1345634e14088921df4e0e5df91ba385413cff
Solomon Hudson <130000339+keenbiscuit@users.noreply.github.com>
2026-09-24 09:27:50 -0500

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        for(char c : s.toCharArray())
+        {
+            if(map.containsKey(c) && !stack.isEmpty())
+            {
+                char curr = stack.pop();
+                if(curr != map.get(c))
+                {
+                    return false;
+                }
+            }
+            else{
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
